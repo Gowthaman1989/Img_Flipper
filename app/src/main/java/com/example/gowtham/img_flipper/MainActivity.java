72f8f221c24023813bbf4a9ff99eb36f810917a6
Gowtham <s.gowthaman1989@gmail.com>
2017-05-30 11:03:22 +0530

    My first project

--- app/src/main/java/com/example/gowtham/img_flipper/MainActivity.java
+++ app/src/main/java/com/example/gowtham/img_flipper/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,60 +15,65 @@ public class MainActivity extends AppCompatActivity {
     Button bu1, bu2, startImg, stopImg, previousImg, nextImg;
     TextView tv1;
 
+    String names[]={"apple","orange","banana","cherries","greenapple"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       final ViewFlipper VF1 = (ViewFlipper) findViewById(R.id.viewflipper1);
+        final ViewFlipper VF1 = (ViewFlipper) findViewById(R.id.viewflipper1);
         VF1.startFlipping();
         VF1.setFlipInterval(500);
         ed1 = (EditText) findViewById(R.id.ed1);
         ed2 = (EditText) findViewById(R.id.ed2);
         bu1 = (Button) findViewById(R.id.bu1);
-        bu2 = (Button) findViewById(R.id.bu2);
-        tv1 = (TextView) findViewById(R.id.tv1);
+                tv1 = (TextView) findViewById(R.id.tv1);
         startImg = (Button) findViewById(R.id.startImg);
         stopImg = (Button) findViewById(R.id.stopImg);
         nextImg = (Button) findViewById(R.id.nextImg);
+
+
         previousImg = (Button) findViewById(R.id.previousImg);
-        startImg.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        startImg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 VF1.startFlipping();
             }
         });
-        stopImg.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v){
+        stopImg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 VF1.stopFlipping();
+
             }
         });
-        previousImg.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v){
+        previousImg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 VF1.showPrevious();
             }
         });
-        nextImg.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v){
+        nextImg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 VF1.showNext();
             }
         });
         bu1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s1 = ed1.getText().toString();
-                tv1.setText(s1);
-                //Gowthaman....
-                VF1.getCurrentView();
+                String fruitName=ed1.getText().toString();
+            Toast.makeText(getApplicationContext(),""+names[2],Toast.LENGTH_LONG).show();
+                for(int i=0;i<names.length;i++)
+                {
+                    if(fruitName.equals(names[i]))
+                    {
+                        Toast.makeText(getApplicationContext(),"Correct!! good",Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
             }
         });
-        bu2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String s2 = ed2.getText().toString();
-                tv1.setText(s2);
-            }
-        });
+
     }
 }
