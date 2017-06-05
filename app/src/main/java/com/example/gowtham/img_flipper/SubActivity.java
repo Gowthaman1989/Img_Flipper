2ed6d484b2c554b087f7553c27cc49c5211a8b79
Gowtham <s.gowthaman1989@gmail.com>
2017-06-05 10:39:41 +0530

    Apple-Kupple

--- app/src/main/java/com/example/gowtham/img_flipper/SubActivity.java
+++ app/src/main/java/com/example/gowtham/img_flipper/SubActivity.java
@@ -14,7 +14,7 @@ public class SubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
         imageView = (ImageView) findViewById(R.id.imageView);
         appleButton = (Button) findViewById(R.id.appleButton);
         bananaButton = (Button) findViewById(R.id.bananaButton);
